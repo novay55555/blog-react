de052423f0dbfc3b0c6bde670deb336a9d9740df
gunhawk <gunhawk998@gmail.com>
2017-05-09 01:19:34 +0800

    增加css格式化任务

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -3,8 +3,10 @@ const prettify = require('gulp-jsbeautifier');
 
 gulp.task('default', ['prettify'], () => {});
 
-gulp.task('prettify', () => 
-  gulp.src(['./src/**/**/*.js'])
+gulp.task('prettify', ['jsprettify', 'cssprettify'], () => {});
+
+gulp.task('jsprettify', () => 
+  gulp.src('./src/**/**/*.js')
     .pipe(prettify({
       'indent_size': 2,
       'e4x': true,
@@ -12,3 +14,12 @@ gulp.task('prettify', () =>
     }))
     .pipe(gulp.dest(file => file.base))
 );
+
+gulp.task('cssprettify', () => 
+  gulp.src('./src/**/**/*.css')
+    .pipe(prettify({
+      'indent_size': 2
+    }))
+    .pipe(gulp.dest(file => file.base))
+);
+
