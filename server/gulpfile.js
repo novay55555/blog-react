0cb77895afc3069177952b0c68b67249aa03e86e
lihaoran <lihaoran@nesting.xyz>
2017-05-17 14:49:56 +0800

    新增gulp任务, 忽略vscode配置文件

---
+++ server/gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const prettify = require('gulp-jsbeautifier');
+
+gulp.task('default', ['prettify'], () => {});
+
+gulp.task('prettify', ['jsprettify'], () => {});
+
+gulp.task('jsprettify', () => 
+  gulp.src(['./**/*.js', '!./node_modules/**'])
+    .pipe(prettify({
+      'indent_size': 2,
+      'brace_style': ',preserve-inline'
+    }))
+    .pipe(gulp.dest(file => file.base))
+);
+
