efae7c8a34a1f471af9428e52a1b800412b57abb
lihaoran <lihaoran@nesting.xyz>
2017-06-01 17:27:26 +0800

    修改部分路由

--- client/src/lib/config.js
+++ client/src/lib/config.js
@@ -10,20 +10,20 @@ const config = {
     articles: {
       lists: page => `/api/articles/${page}`,
       current: id => `/api/article/${id}`,
-      types: `/api/types/articles`,
-      searchByTitle: (title, page) => `/api/search/title/${title}/${page}`,
-      searchByType: (type, page) => `/api/search/type/${type}/${page}`,
+      types: `/api/articles/types`,
+      searchByTitle: (title, page) => `/api/articles/search/title/${title}/${page}`,
+      searchByType: (type, page) => `/api/articles/search/type/${type}/${page}`,
       inside: page => `/api/inside/articles/${page}`,
-      insideSearchByTitle: (title, page) => `/api/inside/search/title/${title}/${page}`,
-      delete: id => `/api/article-delete/${id}`,
-      add: `/api/article-publish`,
-      edit: id => `/api/article-update/${id}`
+      insideSearchByTitle: (title, page) => `/api/inside/articles/search/title/${title}/${page}`,
+      delete: id => `/api/inside/article/delete/${id}`,
+      add: `/api/inside/article/publish`,
+      edit: id => `/api/inside/article/update/${id}`
     },
     users: {
       lists: page => `/api/inside/users/${page}`,
       searchByName: (name, page) => `/api/inside/users/search/${name}/${page}`,
-      edit: `/api/user-edit`,
-      delete: id => `/api/user-delete/${id}`
+      edit: `/api/inside/user/edit`,
+      delete: id => `/api/inside/user/delete/${id}`
     },
     blog: `/api/inside/blog`,
     admin: `/api/inside/admin`
