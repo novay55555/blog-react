8a5470b6daa83736794dfaf992498aa4183bfbdb
lihaoran <lihaoran@nesting.xyz>
2017-06-14 16:23:45 +0800

    新增了api文档构建

--- server/api/index.js
+++ server/api/index.js
@@ -12,7 +12,16 @@ const config = require('../lib/config');
 const apiStatus = config.api;
 
 /**
- * @callback 获取文章类型
+ * @api {get} /api/articles/types 获取文章类型
+ * @apiGroup Articles
+ * @apiName Get article types
+ * @apiSuccess {Array} content 文章类型
+ * @apiSuccessExample {json} Example:
+ * {
+ *  code: 1,
+ *  content: ['javascript', 'nodejs']
+ * }
+ * @apiVersion 1.0.0
  */
 Api.get('/api/articles/types', (req, res) => {
   ArticleTypes.find((err, types) => {
@@ -22,9 +31,41 @@ Api.get('/api/articles/types', (req, res) => {
 });
 
 /**
- * @callback 获取文章
- * 
- * @param {number} page 页码数
+ * @api {get} /api/articles/:page 获取文章
+ * @apiGroup Articles
+ * @apiName Get articles by page
+ * @apiParam {Number} page 页码数
+ * @apiSuccess {Object} content 结果集
+ * @apiSuccess {Number} content.total 总页数
+ * @apiSuccess {Number} content.page 当前页数
+ * @apiSuccess {Object[]} content.articles 文章列表
+ * @apiSuccess {Number} content.articles._id 文章id
+ * @apiSuccess {String} content.articles.title 标题
+ * @apiSuccess {String} content.articles.author 作者
+ * @apiSuccess {Number} content.articles.date 文章更新的时间戳
+ * @apiSuccess {String} content.articles.description 文章描述
+ * @apiSuccess {String} content.articles.articleType 文章类型
+ * @apiSuccess {String} content.articles.content 文章内容
+ * @apiSuccessExample {json} Example:
+ * {
+ *  code: 1,
+ *  content: {
+ *    total: 258  
+ *    page: 1 
+ *    articles: [
+ *      {
+ *        _id: 262, 
+ *        title: '列表测试',  
+ *        author: 'admin',  
+ *        date: 1496880000000,  
+ *        articleType: 'javascript' 
+ *        description: '列表测试列表测试列表测试',  
+ *        content: '列表测试列表测试列表测试'
+ *      }
+ *    ]
+ *  }
+ * }
+ * @apiVersion 1.0.0
  */
 Api.get('/api/articles/:page', (req, res) => {
   const page = parseInt(req.params.page);
@@ -52,9 +93,19 @@ Api.get('/api/articles/:page', (req, res) => {
 
 
 /**
- * @callback 获取文章内容
- * 
- * @param {number} id 文章id
+ * @api {get} /api/article/:id 获取文章内容
+ * @apiGroup Articles
+ * @apiName Get an article by id
+ * @apiParam {Number} id 文章id
+ * @apiSuccess {Object} content 结果集
+ * @apiSuccess {Number} content._id 文章id
+ * @apiSuccess {String} content.title 标题
+ * @apiSuccess {String} content.author 作者
+ * @apiSuccess {Number} content.date 文章更新的时间戳
+ * @apiSuccess {String} content.description 文章描述
+ * @apiSuccess {String} content.articleType 文章类型
+ * @apiSuccess {String} content.content 文章内容
+ * @apiVersion 1.0.0
  */
 Api.get('/api/article/:id', function (req, res) {
   Article.findById(req.params.id, function (err, article) {
@@ -64,10 +115,23 @@ Api.get('/api/article/:id', function (req, res) {
 });
 
 /**
- * @callback 文章搜索(标题)
- * 
- * @param {string} title 搜索的标题
- * @param {number} page 分页页码
+ * @api {get} /api/articles/search/title/:title/:page 文章搜索(标题)
+ * @apiGroup Articles
+ * @apiName Get articles by searching title
+ * @apiParam {String} title 搜索的标题
+ * @apiParam {Number} page 分页页码
+ * @apiSuccess {Object} content 结果集
+ * @apiSuccess {Number} content.total 总页数
+ * @apiSuccess {Number} content.page 当前页数
+ * @apiSuccess {Object[]} content.articles 文章列表
+ * @apiSuccess {Number} content.articles._id 文章id
+ * @apiSuccess {String} content.articles.title 标题
+ * @apiSuccess {String} content.articles.author 作者
+ * @apiSuccess {Number} content.articles.date 文章更新的时间戳
+ * @apiSuccess {String} content.articles.description 文章描述
+ * @apiSuccess {String} content.articles.articleType 文章类型
+ * @apiSuccess {String} content.articles.content 文章内容
+ * @apiVersion 1.0.0
  */
 Api.get('/api/articles/search/title/:title/:page', (req, res) => {
   const page = parseInt(req.params.page);
@@ -95,10 +159,23 @@ Api.get('/api/articles/search/title/:title/:page', (req, res) => {
 });
 
 /**
- * @callback 文章搜索(类型)
- * 
- * @param {string} type 搜索的类型
- * @param {number} page 分页页码
+ * @api {get} /api/articles/search/type/:type/:page 文章搜索(类型)
+ * @apiGroup Articles
+ * @apiName Get articles by searching type
+ * @apiParam {String} type 搜索的类型
+ * @apiParam {Number} page 分页页码
+ * @apiSuccess {Object} content 结果集
+ * @apiSuccess {Number} content.total 总页数
+ * @apiSuccess {Number} content.page 当前页数
+ * @apiSuccess {Object[]} content.articles 文章列表
+ * @apiSuccess {Number} content.articles._id 文章id
+ * @apiSuccess {String} content.articles.title 标题
+ * @apiSuccess {String} content.articles.author 作者
+ * @apiSuccess {Number} content.articles.date 文章更新的时间戳
+ * @apiSuccess {String} content.articles.description 文章描述
+ * @apiSuccess {String} content.articles.articleType 文章类型
+ * @apiSuccess {String} content.articles.content 文章内容
+ * @apiVersion 1.0.0
  */
 Api.get('/api/articles/search/type/:type/:page', (req, res) => {
   const page = parseInt(req.params.page);
@@ -126,10 +203,16 @@ Api.get('/api/articles/search/type/:type/:page', (req, res) => {
 });
 
 /**
- * @callback 登录
- * 
- * @param {string} name        用户名
- * @param {string} password    密码
+ * @api {post} /api/login 登录
+ * @apiGroup Account
+ * @apiName Sign in by account
+ * @apiParam {String} name        用户名
+ * @apiParam {String} password    密码
+ * @apiSuccess {Object} content 结果集
+ * @apiSuccess {String} content.username 用户名
+ * @apiSuccess {Boolean} content.isAdmin 是否为管理员身份
+ * @apiSuccess {Boolean} content.isLogin 是否已登录
+ * @apiVersion 1.0.0
  */
 Api.post('/api/login', (req, res) => {
   User.find({ name: req.body.name, password: req.body.password }, (err, user) => {
@@ -150,7 +233,15 @@ Api.post('/api/login', (req, res) => {
 });
 
 /**
- * @callback 登出
+ * @api {get} /api/signout 登出
+ * @apiGroup Account
+ * @apiName Sign out
+ * @apiSuccessExample {json} Example:
+ * {
+ *  code: 1,
+ *  content: {}
+ * }
+ * @apiVersion 1.0.0
  */
 Api.get('/api/signout', (req, res) => {
   delete req.session.isLogin;
@@ -160,7 +251,14 @@ Api.get('/api/signout', (req, res) => {
 });
 
 /**
- * @callback 验证session
+ * @api {get} /api/checkout 验证session
+ * @apiGroup Account
+ * @apiName Check out an account session is actived or not
+ * @apiSuccess {Object} content 结果集
+ * @apiSuccess {String} content.username 用户名
+ * @apiSuccess {Boolean} content.isAdmin 是否为管理员身份
+ * @apiSuccess {Boolean} content.isLogin 是否已登录
+ * @apiVersion 1.0.0
  */
 Api.get('/api/checkout', (req, res) => {
   req.session.isLogin ? res.json({
@@ -174,11 +272,17 @@ Api.get('/api/checkout', (req, res) => {
 });
 
 /**
- * @callback 注册
- * 
- * @param {string} name        用户名
- * @param {string} password    密码
- * @param {string} email       邮箱
+ * @api {post} /api/register 注册
+ * @apiGroup Account
+ * @apiName Register an account
+ * @apiParam {String} name        用户名
+ * @apiParam {String} password    密码
+ * @apiParam {String} email       邮箱
+ * @apiSuccess {Object} content 结果集
+ * @apiSuccess {String} content.username 用户名
+ * @apiSuccess {Boolean} content.isAdmin 是否为管理员身份
+ * @apiSuccess {Boolean} content.isLogin 是否已登录
+ * @apiVersion 1.0.0
  */
 Api.post('/api/register', (req, res) => {
   User.find({ name: req.body.name }, (err, user) => {
@@ -228,9 +332,22 @@ Api.post('/api/register', (req, res) => {
 });
 
 /**
- * @callback 后台获取文章列表
- * 
- * @param {number} page 分页页码
+ * @api {get} /api/inside/articles/:page 后台获取文章列表
+ * @apiGroup Inside
+ * @apiName Get articles by page
+ * @apiSuccess {Object} content 结果集
+ * @apiSuccess {Number} content.total 总页数
+ * @apiSuccess {Number} content.page 当前页数
+ * @apiSuccess {Object[]} content.articles 文章列表
+ * @apiSuccess {Number} content.articles._id 文章id
+ * @apiSuccess {String} content.articles.title 标题
+ * @apiSuccess {String} content.articles.author 作者
+ * @apiSuccess {Number} content.articles.date 文章更新的时间戳
+ * @apiSuccess {String} content.articles.description 文章描述
+ * @apiSuccess {String} content.articles.articleType 文章类型
+ * @apiSuccess {String} content.articles.content 文章内容
+ * @apiParam {Number} page 分页页码
+ * @apiVersion 1.0.0
  */
 Api.get('/api/inside/articles/:page', (req, res) => {
   const page = parseInt(req.params.page);
@@ -262,10 +379,23 @@ Api.get('/api/inside/articles/:page', (req, res) => {
 });
 
 /**
- * @callback 后台搜索文章列表
- * 
- * @param {string} title 搜索的标题
- * @param {number} page 分页页码
+ * @api {get} /api/inside/articles/search/title/:title/:page 后台搜索文章列表
+ * @apiGroup Inside
+ * @apiName Get articles by searching title
+ * @apiParam {String} title 搜索的标题
+ * @apiParam {Number} page 分页页码
+ * @apiSuccess {Object} content 结果集
+ * @apiSuccess {Number} content.total 总页数
+ * @apiSuccess {Number} content.page 当前页数
+ * @apiSuccess {Object[]} content.articles 文章列表
+ * @apiSuccess {Number} content.articles._id 文章id
+ * @apiSuccess {String} content.articles.title 标题
+ * @apiSuccess {String} content.articles.author 作者
+ * @apiSuccess {Number} content.articles.date 文章更新的时间戳
+ * @apiSuccess {String} content.articles.description 文章描述
+ * @apiSuccess {String} content.articles.articleType 文章类型
+ * @apiSuccess {String} content.articles.content 文章内容
+ * @apiVersion 1.0.0
  */
 Api.get('/api/inside/articles/search/title/:title/:page', (req, res) => {
   const page = parseInt(req.params.page);
@@ -298,15 +428,24 @@ Api.get('/api/inside/articles/search/title/:title/:page', (req, res) => {
 });
 
 /**
- * @callback 文章发表
- * 
- * @param {object} req.body 文章修改的数据
- * @param {string} req.body.title 标题
- * @param {string} req.body.author 作者
- * @param {string} req.body.date 更新时间
- * @param {string} req.body.description 描述
- * @param {string} req.body.articleType 文章类型
- * @param {string} req.body.content 文章内容
+ * @api {get} /api/inside/article/publish 文章发表
+ * @apiGroup Inside
+ * @apiName Publish an article
+ * @apiParam {String} title 标题
+ * @apiParam {String} author 作者
+ * @apiParam {String} date 更新时间
+ * @apiParam {String} description 描述
+ * @apiParam {String} articleType 文章类型
+ * @apiParam {String} content 文章内容
+ * @apiSuccess {Object} content 结果集
+ * @apiSuccess {Number} content._id 文章id
+ * @apiSuccess {String} content.title 标题
+ * @apiSuccess {String} content.author 作者
+ * @apiSuccess {Number} content.date 文章更新的时间戳
+ * @apiSuccess {String} content.description 文章描述
+ * @apiSuccess {String} content.articleType 文章类型
+ * @apiSuccess {String} content.content 文章内容
+ * @apiVersion 1.0.0
  */
 Api.post('/api/inside/article/publish', (req, res) => {
   for (let x in req.body) {
@@ -324,16 +463,22 @@ Api.post('/api/inside/article/publish', (req, res) => {
 });
 
 /**
- * @callback 修改文章
- * 
- * @param {number} id 文章id
- * @param {object} req.body 文章修改的数据
- * @param {string} req.body.title 标题
- * @param {string} req.body.author 作者
- * @param {string} req.body.date 更新时间
- * @param {string} req.body.description 描述
- * @param {string} req.body.articleType 文章类型
- * @param {string} req.body.content 文章内容
+ * @api {post} /api/inside/article/update/:id 修改文章
+ * @apiGroup Inside
+ * @apiName Update an article
+ * @apiParam {Number} id 文章id
+ * @apiParam {String} title 标题
+ * @apiParam {String} author 作者
+ * @apiParam {String} date 更新时间
+ * @apiParam {String} description 描述
+ * @apiParam {String} articleType 文章类型
+ * @apiParam {String} content 文章内容
+ * @apiSuccessExample {json} Example:
+ * {
+ *  code: 1,
+ *  content: {}
+ * }
+ * @apiVersion 1.0.0
  */
 Api.post('/api/inside/article/update/:id', (req, res) => {
   checkAdmin(req.session).then(() => {
@@ -346,16 +491,22 @@ Api.post('/api/inside/article/update/:id', (req, res) => {
       content: req.body.content
     }, (err, raw) => {
       if (err) return res.json({ code: apiStatus.databaseError.code, msg: apiStatus.databaseError.msg });
-      console.log(`'The raw response from Mongo was ${raw}`);
       res.json({ code: apiStatus.success.code, content: {} })
     });
   }).catch(err => res.json(err));
 });
 
 /**
- * @callback 删除文章
- * 
- * @param {number}  id  文章id
+ * @api {get} /api/inside/article/delete/:id 删除文章
+ * @apiGroup Inside
+ * @apiName Delete an article
+ * @apiParam {Number}  id  文章id
+ * @apiSuccessExample {json} Example:
+ * {
+ *  code: 1,
+ *  content: {}
+ * }
+ * @apiVersion 1.0.0
  */
 Api.get('/api/inside/article/delete/:id', (req, res) => {
   checkAdmin(req.session).then(() => {
@@ -367,9 +518,19 @@ Api.get('/api/inside/article/delete/:id', (req, res) => {
 });
 
 /**
- * @callback 获取用户列表
- * 
- * @param {number} page 分页页码
+ * @api {get} /api/inside/users/:page 获取用户列表
+ * @apiGroup Inside
+ * @apiName Get users by page
+ * @apiParam {Number} page 分页页码
+ * @apiSuccess {Object} content 结果集
+ * @apiSuccess {Number} content.page 当前页码
+ * @apiSuccess {Number} content.total 当前页码
+ * @apiSuccess {Object[]} content.users 当前页码
+ * @apiSuccess {String} content.users.email 邮箱
+ * @apiSuccess {String} content.users.name 用户名
+ * @apiSuccess {String} content.users.password 密码
+ * @apiSuccess {Number} content.users.role 角色
+ * @apiVersion 1.0.0
  */
 Api.get('/api/inside/users/:page', (req, res) => {
   const page = parseInt(req.params.page);
@@ -400,11 +561,18 @@ Api.get('/api/inside/users/:page', (req, res) => {
 });
 
 /**
- * @callback 编辑用户密码, 邮箱
- * 
- * @param {number}  id         用户id
- * @param {string}  password   密码
- * @param {string}  email      邮箱
+ * @api {post} /api/inside/user/edit 编辑用户密码, 邮箱
+ * @apiGroup Inside
+ * @apiName Update a user password & email
+ * @apiParam {Number}  id         用户id
+ * @apiParam {String}  password   密码
+ * @apiParam {String}  email      邮箱
+ * @apiSuccessExample 
+ * {
+ *  code: 1,
+ *  content: {}
+ * }
+ * @apiVersion 1.0.0
  */
 Api.post('/api/inside/user/edit', (req, res) => {
   checkAdmin(req.session).then(() => {
@@ -419,9 +587,16 @@ Api.post('/api/inside/user/edit', (req, res) => {
 });
 
 /**
- * @callback 删除用户
- * 
- * @param {number}  id  用户id
+ * @api {get} /api/inside/user/delete/:id 删除用户
+ * @apiGroup Inside
+ * @apiName Delete a user
+ * @apiParam {Number}  id  用户id
+ * @apiSuccessExample 
+ * {
+ *  code: 1,
+ *  content: {}
+ * }
+ * @apiVersion 1.0.0
  */
 Api.get('/api/inside/user/delete/:id', (req, res) => {
   checkAdmin(req.session).then(() => {
@@ -433,10 +608,20 @@ Api.get('/api/inside/user/delete/:id', (req, res) => {
 });
 
 /**
- * @callback 搜索用户
- * 
- * @param {string} name   用户名
- * @param {number} page   分页页码
+ * @api {get} /api/inside/users/search/:name/:page 搜索用户
+ * @apiGroup Inside
+ * @apiName Get users by searching name
+ * @apiParam {String} name   用户名
+ * @apiParam {Number} page 分页页码
+ * @apiSuccess {Object} content 结果集
+ * @apiSuccess {Number} content.page 当前页码
+ * @apiSuccess {Number} content.total 当前页码
+ * @apiSuccess {Object[]} content.users 当前页码
+ * @apiSuccess {String} content.users.email 邮箱
+ * @apiSuccess {String} content.users.name 用户名
+ * @apiSuccess {String} content.users.password 密码
+ * @apiSuccess {Number} content.users.role 角色
+ * @apiVersion 1.0.0
  */
 Api.get('/api/inside/users/search/:name/:page', (req, res) => {
   const page = parseInt(req.params.page);
@@ -468,16 +653,22 @@ Api.get('/api/inside/users/search/:name/:page', (req, res) => {
 });
 
 /**
- * @callback 更新博客
- * 
- * @param {object} req.body 博客更新的数据
- * @param {object} req.body.admin 管理员帐号数据
- * @param {string} req.body.admin.name 帐号
- * @param {string} req.body.admin.password 密码(可选)
- * @param {string} req.body.admin.email 邮箱
- * @param {object} req.body.types 文章类型数据
- * @param {string} req.body.types.id 文章类型的ObjectId
- * @param {array} req.body.types.data 文章类型集合
+ * @api {post} /api/inside/blog 更新博客
+ * @apiGroup Inside
+ * @apiName Update blog settings
+ * @apiParam (group) {object} admin 管理员帐号数据
+ * @apiParam {String} admin.name 帐号
+ * @apiParam {String} admin.password 密码(可选)
+ * @apiParam {String} admin.email 邮箱
+ * @apiParam (group) {object} types 文章类型数据
+ * @apiParam {String} types.id 文章类型的ObjectId
+ * @apiParam {array} types.data 文章类型集合 
+ * @apiSuccessExample 
+ * {
+ *  code: 1,
+ *  content: {}
+ * }
+ * @apiVersion 1.0.0
  */
 Api.post('/api/inside/blog', (req, res) => {
   checkAdmin(req.session).then(() => {
@@ -507,7 +698,15 @@ Api.post('/api/inside/blog', (req, res) => {
 });
 
 /**
- * @callback 获取管理员的相关信息
+ * @api {get} /api/inside/admin 获取管理员的相关信息
+ * @apiGroup Inside
+ * @apiName Get admin info
+ * @apiSuccess {Object} content 结果集
+ * @apiSuccess {String} content.email 邮箱
+ * @apiSuccess {String} content.name 用户名
+ * @apiSuccess {String} content.password 密码
+ * @apiSuccess {Number} content.role 角色
+ * @apiVersion 1.0.0
  */
 Api.get('/api/inside/admin', (req, res) => {
   checkAdmin(req.session).then(() => {
@@ -521,7 +720,7 @@ Api.get('/api/inside/admin', (req, res) => {
 module.exports = Api;
 
 /**
- * @callback 后台管理员身份验证
+ * @function 后台管理员身份验证
  * 
  * @param {object} session 用户session对象
  * @returns {Promise} Promise对象
